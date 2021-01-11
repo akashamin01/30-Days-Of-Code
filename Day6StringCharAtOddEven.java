b14254ceebefd219ff11f8cec6fdc0d5a39a5172
Akash Amin <38359324+akashamin01@users.noreply.github.com>
2021-01-11 16:28:42 +0530

    Update Day6StringCharAtOddEven.java

--- Day6StringCharAtOddEven.java
+++ Day6StringCharAtOddEven.java
@@ -9,19 +9,19 @@ public class Solution {
         int n = sc.nextInt();
         String s = sc.nextLine();
         for(int x=0;x<n;x++){
-        String s1 = sc.nextLine();
-        char[] ch1 = s1.toCharArray();                                         
-        StringBuilder sb1e = new StringBuilder(); 
-        StringBuilder sb1o = new StringBuilder();      
-        for(int i=0; i<s1.length(); i++){
-            if(i%2 == 0){   
-                sb1e.append(ch1[i]);                            
-            }else{  
-                sb1o.append(ch1[i]);              
-            }
-        }    
-        // System.out.println(n);
-        System.out.println(sb1e.toString()+" "+sb1o.toString());     
-    }
+            String s1 = sc.nextLine();
+            char[] ch1 = s1.toCharArray();                                         
+            StringBuilder sb1e = new StringBuilder(); 
+            StringBuilder sb1o = new StringBuilder();      
+            for(int i=0; i<s1.length(); i++){
+                if(i%2 == 0){   
+                    sb1e.append(ch1[i]);                            
+                }else{  
+                    sb1o.append(ch1[i]);              
+                }
+            }    
+            // System.out.println(n);
+            System.out.println(sb1e.toString()+" "+sb1o.toString());     
+        }
     }
 }
