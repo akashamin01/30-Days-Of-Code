df56f47b8bf4d914a80d93dba22aa6bb944630bf
Akash Amin <38359324+akashamin01@users.noreply.github.com>
2021-02-01 23:37:06 +0530

    Create Day28RegEX.java

---
+++ Day28RegEX.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        List<String> userlist = new ArrayList<String>();
+        
+        while( i-- >0 ){
+            String name = scan.next();
+            String email = scan.next();
+                       
+            boolean isnamelength = Pattern.matches("\\w{1,20}", name);
+            boolean isname = Pattern.matches("^[a-z]*$", name);            
+                        
+            boolean ismaillength = Pattern.matches("\\D{1,50}", email);
+            boolean isgmail = email.endsWith("@gmail.com");
+            
+            if(isnamelength && isname && ismaillength && isgmail){
+                userlist.add(name);
+            }
+        }
+        
+        String[] arr = userlist.toArray(new String[userlist.size()]);
+        Arrays.sort(arr);
+        for(int j = 0 ; j<userlist.size(); j++){
+            System.out.println(arr[j]);
+        }
+    }
+}
